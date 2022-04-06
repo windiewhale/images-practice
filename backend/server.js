e1f2794f96d19a8761191dda253caf8fbc3c2df5
windiewhale <feketebodza@gmail.com>
2022-04-06 16:35:10 +0200

    swipe yay

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const port = 9000;
+const feFolder = `${__dirname}/../frontend`
+const frontEndPath = path.join(`${__dirname}/../frontend`)
+
+
+app.use(express.json())
+
+app.get("/", (req, res, next) => {
+    res.sendFile(`${frontEndPath}/index.html`)
+})
+
+app.get("/image-list", (req, res) => {
+    res.sendFile(`${frontEndPath}/data.json`)
+})
+
+app.use("/pub", express.static(`${feFolder}/public`));
+
+app.listen(port, () => {
+    console.log(`http://127.0.0.1:${port}`);
+})
---
+++ frontend/public/script.js
@@ -0,0 +1,39 @@
+const parseJSON = async (url) => {
+    const response = await fetch(url);
+    return response.json()
+}
+
+const swiperComponent = (data, component) => {
+    return `
+    <div class="swiper">
+        <div class="swiper-wrapper">
+        ${data.map(img => component(img)).join("")}   
+        </div>
+    </div>
+    `
+}
+
+const swiperSlideComponent = ({filename, title}) => {
+    return `
+    <div class="swiper-slide">
+        <h2>${title}</h2>
+        <img src="/pub/images/${filename}">
+    </div>
+    `
+}
+
+const loadEvent = async () => {
+    const root = document.getElementById("root")
+    const result = await parseJSON("/image-list")
+    
+    root.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent))
+
+    const swiper = new Swiper(".swiper", {
+        loop: true
+    })
+
+    console.log(swiper);
+
+}
+
+window.addEventListener("load", loadEvent)
